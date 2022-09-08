eeb466a031a5c9062709d938dd50a59f123169f1
Sergei Iakimov <>
2022-09-08 09:23:38 +0000

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ({ selectedTeam, teamMemberCount }) => {
+  return (
+    <header>
+      <h1>Team Member Allocation</h1>
+      <h3>{selectedTeam} has {teamMemberCount} Members</h3>
+    </header>
+  )
+}
+export default Header
